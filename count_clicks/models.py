70223a85459b34f7ccf76df4f16b852e61ca122e
Lim Taegyu <dlawork9888@gmail.com>
2024-05-30 17:40:53 +0900

    Model: DateField => DateTimeField

--- count_clicks/models.py
+++ count_clicks/models.py
@@ -7,7 +7,7 @@ from django.db import models
  
 class ClickModel(models.Model):
     count_id = models.AutoField(primary_key =  True)
-    clicked_date = models.DateField(auto_now_add = True) # POST 요청이 들어오면 현재 날짜가 자동으로 생성됨!
+    clicked_date = models.DateTimeField(auto_now_add = True) # POST 요청이 들어오면 현재 날짜가 자동으로 생성됨!
 
     def __str__(self):
         return f"Click {self.count_id} on {self.clicked_date}"
